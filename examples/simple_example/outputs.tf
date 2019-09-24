72dc95f216ab615dbb1b308c423c570f85b61fca
Olivier Cervello <ocervello@google.com>
2019-09-24 11:22:46 +0200

    Added comment for submodule outputs

--- examples/simple_example/outputs.tf
+++ examples/simple_example/outputs.tf
@@ -14,6 +14,10 @@
  * limitations under the License.
  */
 
+# Note: As most of the submodules outputs are needed: we are just forwarding all
+# submodules outputs here. Please refer to the submodules outputs.tf file to
+# have a breakdown.
+
 output "slo_pipeline" {
   description = "SLO pipeline outputs"
   value       = module.slo-pipeline
