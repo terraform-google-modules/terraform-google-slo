987f5b5160bdb4ab5bec578c66839ce1a5100560
Benjamin Kaminski <benjamin.kaminski@decathlon.com>
2020-03-11 16:46:57 +0100

    Update main.tf

--- modules/slo-pipeline/variables.tf
+++ modules/slo-pipeline/variables.tf
@@ -88,6 +88,12 @@ variable "function_source_directory" {
   default     = ""
 }
 
+variable "dataset_default_table_expiration_ms" {
+  type        = string
+  description = "(Optional) The default lifetime of the slo table in the dataset, in milliseconds. Default is 1 year (86400000)"
+  default     = "86400000"
+}
+
 variable "slo_generator_version" {
   description = "SLO generator library version"
   default     = "0.1.7"
