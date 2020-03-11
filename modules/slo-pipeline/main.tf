2b6d4f85472909d8f17bcca1ff6ade8685de4866
Benjamin Kaminski <benjamin.kaminski@decathlon.com>
2020-03-11 16:46:57 +0100

    Update main.tf

--- modules/slo-pipeline/main.tf
+++ modules/slo-pipeline/main.tf
@@ -52,7 +52,7 @@ resource "google_bigquery_dataset" "main" {
   location                    = lookup(local.bigquery_configs[count.index], "location", "EU")
   friendly_name               = "SLO Reports"
   description                 = "Table storing SLO reports from SLO reporting pipeline"
-  default_table_expiration_ms = 525600000 # 1 year
+  default_table_expiration_ms = var.dataset_default_table_expiration_ms
 }
 
 module "event_function" {
