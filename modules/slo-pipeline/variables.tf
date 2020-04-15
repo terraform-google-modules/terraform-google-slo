5cfcbb3224cb7faae4fe2ac2772f743c7e73717f
Olivier Cervello <ocervello@google.com>
2020-04-15 11:48:55 +0200

    Update slo-generator to v1.0.0 + fix default_table_expiration_ms variable

--- modules/slo-pipeline/variables.tf
+++ modules/slo-pipeline/variables.tf
@@ -89,9 +89,9 @@ variable "function_source_directory" {
 }
 
 variable "dataset_default_table_expiration_ms" {
-  type        = string
+  type = number
   description = "The default lifetime of the slo table in the dataset, in milliseconds. Default is never (Recommended)"
-  default     = ""
+  default     = null
 }
 
 variable "slo_generator_version" {
