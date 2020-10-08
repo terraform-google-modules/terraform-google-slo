9bf4d74a518ec8a8bf8bf55d16df7edebc5279f9
Olivier Cervello <ocervello@google.com>
2020-10-08 18:22:46 +0200

    Upgrade slo-generator version
    slo-generator had a broken dependency not pinned: this will upgrade existing installations to slo-generator 1.1.2 where it has been patched.

--- modules/slo/variables.tf
+++ modules/slo/variables.tf
@@ -35,7 +35,7 @@ variable "labels" {
 
 variable "slo_generator_version" {
   description = "SLO generator library version"
-  default     = "1.0.1"
+  default     = "1.1.2"
 }
 
 variable "config" {
