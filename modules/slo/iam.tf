4d631101471d5209789a8c846a8fb9062558e256
Olivier Cervello <ocervello@google.com>
2020-10-16 14:39:12 +0200

    Fix IAM

--- modules/slo/iam.tf
+++ modules/slo/iam.tf
@@ -18,7 +18,7 @@ locals {
   sa_name            = var.service_account_name != "" ? var.service_account_name : local.full_name
   sa_email           = length(google_service_account.main) > 0 ? google_service_account.main[0].email : var.service_account_email
   ssm_class          = var.config.backend.class == "StackdriverServiceMonitoring"
-  ssm_project_id     = lookup(lookup(var.config.backend.measurement, "cluster_istio", {}), "project_id", "")
+  ssm_project_id     = lookup(lookup(lookup(var.config.backend, "measurement", {}), "cluster_istio", {}), "project_id", "")
   sd_class           = var.config.backend.class == "Stackdriver"
   backend_project_id = lookup(var.config.backend, "project_id", null)
 }
