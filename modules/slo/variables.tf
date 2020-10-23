fdf23e99c3a34e767757dc68c9380070d68ebfa2
Olivier Cervello <ocervello@google.com>
2020-10-23 16:28:13 +0200

    Update slo-generator to v1.3.1

--- modules/slo/variables.tf
+++ modules/slo/variables.tf
@@ -35,7 +35,7 @@ variable "labels" {
 
 variable "slo_generator_version" {
   description = "SLO generator library version"
-  default     = "1.2.0"
+  default     = "1.3.1"
 }
 
 variable "extra_files" {
