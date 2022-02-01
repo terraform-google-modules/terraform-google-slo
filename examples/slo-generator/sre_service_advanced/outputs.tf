187c77015689f5d6f58cafa6fedfd9acb41ae875
Olivier Cervello <ocervello@google.com>
2022-02-01 16:13:19 +0100

    update

--- examples/slo-generator/sre_service_advanced/outputs.tf
+++ examples/slo-generator/sre_service_advanced/outputs.tf
@@ -18,6 +18,14 @@ output "slo-generator" {
   value = module.slo-generator
 }
 
+output "team1-slos" {
+  value = module.team1-slos
+}
+
+output "team2-slos" {
+  value = module.team2-slos
+}
+
 output "service_account_email" {
   value = google_service_account.service_account.email
 }
