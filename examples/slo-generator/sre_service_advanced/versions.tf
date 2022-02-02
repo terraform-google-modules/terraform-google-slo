bdb49f012deb09f6ab04f609ad7d6a4c49a81a25
Olivier Cervello <ocervello@google.com>
2022-02-02 15:52:06 +0100

    update versions.tf

--- examples/slo-generator/sre_service_advanced/versions.tf
+++ examples/slo-generator/sre_service_advanced/versions.tf
@@ -18,12 +18,14 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "~> 4.0"
     }
     google-beta = {
-      source = "hashicorp/google-beta"
+      source  = "hashicorp/google-beta"
       version = "~> 4.0"
     }
   }
 }
+
+
